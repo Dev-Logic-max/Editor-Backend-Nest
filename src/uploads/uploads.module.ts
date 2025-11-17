a4a623a52571326fee09ed43ab1a666cf2dff718
Dev-Logic-max <ar3991492@gmail.com>
2025-11-17 20:22:01 +0500

    Uploads module import fix

--- src/uploads/uploads.module.ts
+++ src/uploads/uploads.module.ts
@@ -4,8 +4,8 @@ import { UsersModule } from 'src/users/users.module';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
-import { UploadsController } from './uploads.controller';
-import { UploadsService } from './uploads.service';
+import { UploadsController } from 'src/uploads/uploads.controller';
+import { UploadsService } from 'src/uploads/uploads.service';
 
 @Module({
   imports: [ConfigModule, UsersModule, DocumentsModule],
